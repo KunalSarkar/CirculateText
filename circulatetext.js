434b7bde6ef7d886cc368543ab317af88d2a0a8c
Bhavesh Gohel <ibhaveshgohel@gmail.com>
2017-11-01 17:00:29 +0530

    Refactor the code

--- circulatetext.js
+++ circulatetext.js
@@ -3,17 +3,16 @@ $.fn.circulateText = function(radius,space=10) {
     return this.each(function() {
 	
 		var text= $(this).text().trim();
+	    	var textLengh = text.length;
 		$(this).empty();
 		
 		$(this).append('<div class="innerWrapper" style="position:relative"></div>');
 		var innerWrapper = $(this).find('.innerWrapper');
 		
 		var stepAngle = space;
-		var startAngle;
+		var startAngle= -(space * textLengh/2)+(space/2);
 		
-		startAngle = -( space * text.length/2)+(space/2);
-		
-		for(i=0; i<text.length; i++)
+		for(i=0; i<textLengh; i++)
 		{
 		    $(innerWrapper).append('<span class="singleChar" style="height:'+radius+'px; position:absolute; left:'+$(innerWrapper).width()/2+'px;">'+text[i]+'</span>');
 		    $(innerWrapper).find('.singleChar').last().rotate({
